5957e4fd543475ef4d7f9fcacfabe6a18369bff5
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-10-10 12:00:32 +0100

    changing title

--- client/src/App.js
+++ client/src/App.js
@@ -37,11 +37,11 @@ const App = () => {
       {!authToken && <Auth/>}
       {authToken &&
         <>
-        <ListHeader listName={'🏝️ Holiday tick list'} getData={getData} />
+        <ListHeader listName={'Task Doer'} getData={getData} />
         <p className="user-email">Welcome back {userEmail}</p>
         {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
         </>}
-      <p className="copyright">© Creative Coding LLC</p>
+      <p className="copyright">Get Things Done</p>
     </div>
   )
 }
