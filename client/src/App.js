9f21447d96f8f9ed2458bdf0278f1e655e9bbfb9
Eugenepython <50318257+Eugenepython@users.noreply.github.com>
2023-10-10 18:17:45 +0100

    changing caddy and nixpacks

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,7 @@ const App = () => {
 
   return (
     <div className="app">
+      
       {!authToken && <Auth/>}
       {authToken &&
         <>
--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -60,7 +60,9 @@ const Auth = () => {
 
 
   return (
+    <>
     <div className="auth-container">
+     
       <div className="auth-container-box">
         <form>
           <h2>{isLogIn  ? 'Please log in' : 'Please sign up!'}</h2>
@@ -95,6 +97,7 @@ const Auth = () => {
 
       </div>
     </div>
+    </>
   )
 }
 
